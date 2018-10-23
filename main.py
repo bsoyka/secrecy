0d594e5ab9db290473aadcaf6337b104429d290b
Benjamin Soyka <bensoyka@icloud.com>
2018-10-23 16:46:55 -0700

    Set up more detail for Sentry
    To aid issue tracking

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import sentry_sdk
-sentry_sdk.init("https://072345714651474581a92b0f9f9f2974@sentry.io/1307216")
+import requests
+sentry_sdk.init(
+    "https://072345714651474581a92b0f9f9f2974@sentry.io/1307216",
+    environment = "prod",
+    send_default_pii = True,
+    release = "secrecy@1.0.0"
+)
+try:
+    user_ip = requests.get("https://api.ipify.org")
+except:
+    pass
+with sentry_sdk.configure_scope() as scope:
+    scope.user = {"ip_address": user_ip}
+)
+
 characters = "'qicyQX$AV?[:eRn(&g!Zk.PtvT+F<GI>\"u\\lho@zpW~KL}N){]B^`Y#*Cj/UwMxOHDfasb,m%|JES;rd_"
 message = input("Message: ")
 key = int(input("Key (number between 1 and {}): ".format(len(characters))))
