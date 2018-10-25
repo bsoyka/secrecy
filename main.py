97564220f511a350639d41a1a17fbcbb2b8baeec
Benjamin Soyka <37779854+bsoyka@users.noreply.github.com>
2018-10-25 14:30:19 -0700

    Add comments

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import sentry_sdk
 import requests
+
+# Error tracking setup
+
+## Initialize the Sentry SDK
+
 sentry_sdk.init(
     "https://072345714651474581a92b0f9f9f2974@sentry.io/1307216",
     environment = "prod",
     send_default_pii = True,
     release = "secrecy@1.0.1"
 )
+
+## Get user IP address
+
 try:
     user_ip = requests.get("https://api.ipify.org")
 except:
     pass
+
+## Set Sentry SDK scope with user IP address
+
 with sentry_sdk.configure_scope() as scope:
     scope.user = {"ip_address": user_ip}
+
+# Start secret message encoder/decoder
 
 characters = "'qicyQX$AV?[:eRn(&g!Zk.PtvT+F<GI>\"u\\lho@zpW~KL}N){]B^`Y#*Cj/UwMxOHDfasb,m%|JES;rd_"
 message = input("Message: ")
